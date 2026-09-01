af9edd2bbc21fa6888a8f4b5e6a02109a72f5e12
carol <msuje3536@gmail.com>
2026-09-01 16:01:36 -0400

    v1

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 
 
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwNvpXpN3KiARnZ64IGt2_DO2dRm1Y3VVytk43weyOYYYHfKZZH2DMptgd6zhsyJhpstg/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzoGd0nxdj9Iyufw_fzsamu9X1iC-v9xXHy2L3m-AXLuGyQEgOtB2l-EN481PgofCK0Dw/exec';
 
 export const ADMIN_PASSWORD = 'admin123';
 export const USER_PASSWORD = 'usuario123';
