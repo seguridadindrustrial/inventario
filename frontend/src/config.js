33c09424297ab8dba2d370c4d8ec7148f2e9d051
carol <msuje3536@gmail.com>
2026-09-03 11:06:30 -0400

    v1

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,6 +2,6 @@
 
 export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbydyjYUnO2zWwemQkHZaEIDGh9qHwpLdRW3-GbA2PTAxBqxJI948mhSgFc61QzrVUtbag/exec';
 
-export const ADMIN_PASSWORD = import.meta.env.VITE_ADMIN_PASSWORD || '';
-export const USER_PASSWORD = import.meta.env.VITE_USER_PASSWORD || '';
-export const SCRIPT_TOKEN = import.meta.env.VITE_TOKEN || '';
+export const ADMIN_PASSWORD = import.meta.env.VITE_CLAVE_ADMIN || '';
+export const USER_PASSWORD = import.meta.env.VITE_CLAVE_USUARIO || '';
+export const SCRIPT_TOKEN = import.meta.env.VITE_CLAVE_TOKEN || '';
