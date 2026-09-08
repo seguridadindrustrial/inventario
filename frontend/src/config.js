2d03a9b881af624d0c0a1fe2f204a61211b0a04c
seguridad industrial <carolinablanco8419@gmail.com>
2026-09-08 10:57:38 -0400

    v1.0

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 
 
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxWSAIXbsOUkOVHDSBLPEdfCEh_2lqGl2lsAzHdAmzgy-1LpVUrmkNAj4wa36UtJvS4pQ/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxzt-0Wo3ZbDmn_LAZsKzLwjoQCJkwCwn0UShO5VFA6kLnZcNBhCXLwRFZ1CLTdzn0MhA/exec';
 
 export const ADMIN_PASSWORD = import.meta.env.VITE_CLAVE_ADMIN || '';
 export const USER_PASSWORD = import.meta.env.VITE_CLAVE_USUARIO || '';
